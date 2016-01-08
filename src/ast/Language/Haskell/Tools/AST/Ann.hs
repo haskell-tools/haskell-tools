74da30136c514df4ab7b291f1c860a4bdcfa765e
nboldi <nboldi@elte.hu>
2016-01-08 13:35:22 +0100

    Formatting and minor changes in annotation handling

--- src/ast/Language/Haskell/Tools/AST/Ann.hs
+++ src/ast/Language/Haskell/Tools/AST/Ann.hs
@@ -5,26 +5,23 @@
 
 module Language.Haskell.Tools.AST.Ann where
 
-newtype AnnList e a       = AnnList [Ann e a]
-newtype AnnMaybe e a      = AnnMaybe (Maybe (Ann e a))
+newtype AnnList e a = AnnList [Ann e a]
+newtype AnnMaybe e a = AnnMaybe (Maybe (Ann e a))
 newtype AnnEither e1 e2 a = AnnEither (Either (Ann e1 a) (Ann e2 a))
 
-annJust       = AnnMaybe . Just
-annNothing    = AnnMaybe Nothing
-annRight      = AnnEither . Right
-annLeft       = AnnEither . Left
-  
+annJust :: Ann e a -> AnnMaybe e a
+annJust = AnnMaybe . Just
+
+annNothing :: AnnMaybe e a
+annNothing = AnnMaybe Nothing
+
+annLeft :: Ann e1 a -> AnnEither e1 e2 a
+annLeft = AnnEither . Left
+
+annRight :: Ann e2 a -> AnnEither e1 e2 a
+annRight = AnnEither . Right
+
 data Ann elem annot
-  = Ann
-    { element      :: elem annot
-    , annotation   :: annot
-    }
-  deriving (Eq, Show)
-
-deriving instance (Eq (Ann e a)) => Eq (AnnList e a)
-deriving instance (Show (Ann e a)) => Show (AnnList e a)
-deriving instance (Eq (Ann e a)) => Eq (AnnMaybe e a)
-deriving instance (Show (Ann e a)) => Show (AnnMaybe e a)
-deriving instance (Eq (Ann e1 a), Eq (Ann e2 a)) => Eq (AnnEither e1 e2 a)
-deriving instance (Show (Ann e1 a), Show (Ann e2 a)) => Show (AnnEither e1 e2 a)
-
+  = Ann { annotation   :: annot
+        , element      :: elem annot
+        }
