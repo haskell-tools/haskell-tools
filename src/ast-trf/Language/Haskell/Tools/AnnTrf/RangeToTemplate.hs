486be8096d493c4f3c5339097c960a9b60d125ec
nboldi <nboldi@elte.hu>
2016-01-11 15:54:19 +0100

    Range cutting implemented

--- src/ast-trf/Language/Haskell/Tools/AnnTrf/RangeToTemplate.hs
+++ src/ast-trf/Language/Haskell/Tools/AnnTrf/RangeToTemplate.hs
@@ -1,3 +1,49 @@
+{-# LANGUAGE ScopedTypeVariables
+           , LambdaCase
+           , FlexibleContexts #-}
 module Language.Haskell.Tools.AnnTrf.RangeToTemplate where
+
+import Language.Haskell.Tools.AST.Ann
+import Language.Haskell.Tools.AST.Instances
+
+import Data.StructuralTraversal
+import Control.Monad.State
+import SrcLoc
+
+data RangeTemplate = RangeTemplate SrcSpan [RangeTemplateElem]
+
+rangeToTemplate :: SrcSpan -> RangeTemplate
+rangeToTemplate span = RangeTemplate span [RangeElem span]
+
+data RangeTemplateElem = RangeElem SrcSpan
+                       | RangeChildElem Int
+
+-- | Creates a source template from the ranges and the input file.
+-- All source ranges must be good ranges.
+cutUpRanges :: forall node . StructuralTraversable node => Ann node SrcSpan -> Ann node RangeTemplate
+cutUpRanges n = evalState (cutUpRanges' n) [[]]
+  where cutUpRanges' :: StructuralTraversable node => Ann node SrcSpan -> State [[SrcSpan]] (Ann node RangeTemplate)
+        cutUpRanges' = structTraverse desc asc f
+        
+        -- keep the stack to contain all ranges under it
+        desc = modify ([]:)
+        asc  = modify (\case (x : [y] : xs) -> (y:x) : xs)
+        
+        -- combine each nodes range info with the top of the stack
+        f ni = do top <- gets head
+                  modify (\case ([] : xs) -> [ni] : xs)
+                  return (cutOutElem ni top)
+
+cutOutElem :: SrcSpan -> [SrcSpan] -> RangeTemplate
+cutOutElem sp = RangeTemplate sp . snd . foldl (\(n, temp) spIn -> (n + 1, (concatMap (\t -> breakUpRangeElem n t spIn) temp))) (1, [RangeElem sp])
+                  
+breakUpRangeElem :: Int -> RangeTemplateElem -> SrcSpan -> [RangeTemplateElem]
+breakUpRangeElem n (RangeElem (RealSrcSpan outer)) (RealSrcSpan inner) 
+  | outer `containsSpan` inner 
+  = [ RangeElem (RealSrcSpan $ mkRealSrcSpan (realSrcSpanStart outer) (realSrcSpanStart inner))
+    , RangeChildElem n
+    , RangeElem (RealSrcSpan $ mkRealSrcSpan (realSrcSpanEnd inner) (realSrcSpanEnd outer))
+    ]
+breakUpRangeElem _ outer _ = [ outer ]
 
 
---
+++ src/ast-trf/Language/Haskell/Tools/AnnTrf/SourceTemplate.hs
@@ -0,0 +1,35 @@
+
+module Language.Haskell.Tools.AnnTrf.SourceTemplate where
+
+-- | A pattern that controls how the original source code can be
+-- retrieved from the AST. A source template is assigned to each node.
+-- It has holes where the content of an other node should be printed.
+type SourceTemplate = [SourceTemplateElem]   
+ 
+data SourceTemplateElem = TextElem String 
+                        | ChildElem Int
+     deriving (Eq, Ord)
+     
+isTextElem :: SourceTemplateElem -> Bool
+isTextElem (TextElem _) = True
+isTextElem _ = False
+
+isChildElem :: SourceTemplateElem -> Bool
+isChildElem (ChildElem _) = True
+isChildElem _ = False
+      
+instance Show SourceTemplateElem where
+  show (TextElem s) = s
+  show (ChildElem ni) = "<" ++ show ni ++ ">"
+     
+getIndexedNode :: SourceTemplateElem -> Maybe NodeIndex
+getIndexedNode (ChildElem ni) = Just ni
+getIndexedNode _ = Nothing
+     
+steCombine :: SourceTemplateElem -> SourceTemplateElem -> SourceTemplateElem
+steCombine (TextElem s1) (TextElem s2) = TextElem (s1 ++ s2)
+steCombine (TextElem "") e = e
+steCombine e (TextElem "") = e
+steCombine e1 e2 = error $ "Source template elements " 
+                             ++ show e1 ++ " and " ++ show e2 
+                             ++ " cannot be combined."
