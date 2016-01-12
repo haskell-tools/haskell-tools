dcbc3fcdae920cb54963148c1ba8ebdc0754f46c
nboldi <nboldi@elte.hu>
2016-01-12 23:13:56 +0100

    Tidied and documented Language.Haskell.Tools.AST.Ann

--- src/ast/Language/Haskell/Tools/AST/Ann.hs
+++ src/ast/Language/Haskell/Tools/AST/Ann.hs
@@ -1,27 +1,29 @@
-{-# LANGUAGE FlexibleContexts
-           , UndecidableInstances
-           , StandaloneDeriving
-           #-}
-
+-- | Parts of AST representation for keeping extra data
 module Language.Haskell.Tools.AST.Ann where
 
+-- | An element of the AST keeping extra information.
 data Ann elem annot
-  = Ann { annotation   :: annot
-        , element      :: elem annot
+-- The type parameters are organized this way because we want the annotation type to
+-- be more flexible, but the annotation is the first parameter because it eases 
+-- pattern matching.
+  = Ann { annotation :: annot -- ^ The extra information for the AST part
+        , element    :: elem annot -- ^ The original AST part
         }
 
+-- | A list of AST elements
 newtype AnnList e a = AnnList { fromAnnList :: [Ann e a] }
 
+-- | An optional AST element
 newtype AnnMaybe e a = AnnMaybe { fromAnnMaybe :: (Maybe (Ann e a)) }
 
+-- | An empty list of AST elements
 annNil :: AnnList e a
 annNil = AnnList []
 
-annCons :: AnnList e a -> Ann e a -> AnnList e a
-annCons (AnnList l) = AnnList . (:l)
-
+-- | An existing AST element
 annJust :: Ann e a -> AnnMaybe e a
 annJust = AnnMaybe . Just
 
+-- | A non-existing AST part
 annNothing :: AnnMaybe e a
 annNothing = AnnMaybe Nothing
