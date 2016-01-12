36e8d7d2be7d831533b31ac3bf10d106d06247f7
nboldi <nboldi@elte.hu>
2016-01-12 16:26:23 +0100

    documentation

--- src/ast/Language/Haskell/Tools/AST/FromGHC/Utils.hs
+++ src/ast/Language/Haskell/Tools/AST/FromGHC/Utils.hs
@@ -1,3 +1,4 @@
+-- | Utility functions for transforming the GHC AST representation into our own.
 module Language.Haskell.Tools.AST.FromGHC.Utils where
 
 import Control.Monad.Reader
@@ -9,18 +10,27 @@ import ApiAnnotation
 import SrcLoc
 import HsSyn
 
+-- | RangeInfo (RI) is an alias for SrcSpan
 type RI = SrcSpan
 
+-- | Transform a located part of the AST by automatically transforming the location.
+-- Sets the source range for transforming children.
 trfLoc :: (a -> Trf (b RI)) -> Located a -> Trf (Ann b RI)
 trfLoc f (L l e) = Ann l <$> local (\s -> s { contRange = l }) (f e) 
 
+-- | Transform a located part of the AST by automatically transforming the location
+-- with correction by applying the given function. Sets the source range for transforming children.
 trfLocCorrect :: (RI -> Trf RI) -> (a -> Trf (b RI)) -> Located a -> Trf (Ann b RI)
 trfLocCorrect locF f (L l e) = do loc <- locF l
                                   Ann loc <$> local (\s -> s { contRange = loc }) (f e)
 
+-- | Transform a located part of the AST by automatically transforming the location.
+-- Sets the source range for transforming children.
 trfMaybeLoc :: (a -> Trf (Maybe (b RI))) -> Located a -> Trf (Maybe (Ann b RI))
 trfMaybeLoc f (L l e) = fmap (Ann l) <$> local (\s -> s { contRange = l }) (f e)  
 
+-- | Transform a located part of the AST by automatically transforming the location.
+-- Sets the source range for transforming children.
 trfListLoc :: (a -> Trf [b RI]) -> Located a -> Trf [Ann b RI]
 trfListLoc f (L l e) = fmap (Ann l) <$> local (\s -> s { contRange = l }) (f e)  
 
@@ -35,12 +45,12 @@ tokensLoc keys = asks contRange >>= tokensLoc' keys
         tokensLoc' (keyw:rest) r 
           = do spanFirst <- fromMaybe noSrcSpan <$> getKeywordInside keyw r <$> asks srcMap
                spanRest <- tokensLoc' rest (mkSrcSpan (srcSpanEnd spanFirst) (srcSpanEnd r))
-               return (combineSrcSpans spanFirst spanRest)
-                                       
+               return (combineSrcSpans spanFirst spanRest)                   
         tokensLoc' [] r = pure r
         
 -- | Searches for a token and retrieves its location anywhere
 uniqueTokenAnywhere :: AnnKeywordId -> Trf RI
 uniqueTokenAnywhere keyw = fromMaybe noSrcSpan <$> (getKeywordAnywhere keyw <$> asks srcMap)
         
+-- | No annotation for a part of the AST
 noAnn = Ann noSrcSpan
--- src/ast/Language/Haskell/Tools/AST/SourceMap.hs
+++ src/ast/Language/Haskell/Tools/AST/SourceMap.hs
@@ -5,7 +5,6 @@ import Data.Map as Map
 import Data.List as List
 import Safe
 
--- import Language.Haskell.Tools.AST as AST
 import SrcLoc as GHC
 import FastString as GHC
 
@@ -16,6 +15,7 @@ type SourceMap = Map AnnKeywordId (Map SrcLoc SrcLoc)
 getKeywordAnywhere :: AnnKeywordId -> SourceMap -> Maybe SrcSpan
 getKeywordAnywhere keyw srcmap = return . uncurry mkSrcSpan =<< headMay . assocs =<< (Map.lookup keyw srcmap)
 
+-- | Get the source location of a token restricted to a certain source span
 getKeywordInside :: AnnKeywordId -> SrcSpan -> SourceMap -> Maybe SrcSpan
 getKeywordInside keyw sr srcmap = getSourceElementInside sr =<< Map.lookup keyw srcmap
 
@@ -26,6 +26,7 @@ getSourceElementInside sr srcmap =
                                          else Nothing
     Nothing -> Nothing
     
+-- | Converts GHC Annotations into a convenient format for looking up tokens
 annotationsToSrcMap :: Map ApiAnnKey [SrcSpan] -> Map AnnKeywordId (Map SrcLoc SrcLoc)
 annotationsToSrcMap anns = Map.map (List.foldr addToSrcRanges Map.empty) $ mapKeysWith (++) snd anns
   where 
