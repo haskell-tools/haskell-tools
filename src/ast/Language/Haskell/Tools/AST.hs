94f290b6684feb28d142f088ab2fd0b0515f63d7
nboldi <nboldi@elte.hu>
2016-01-24 15:38:42 +0100

    created wrapper modules

---
+++ src/ast/Language/Haskell/Tools/AST.hs
@@ -0,0 +1,16 @@
+module Language.Haskell.Tools.AST 
+  ( module Language.Haskell.Tools.AST.Ann
+  , module Language.Haskell.Tools.AST.Base
+  , module Language.Haskell.Tools.AST.Literals
+  , module Language.Haskell.Tools.AST.Decl
+  , module Language.Haskell.Tools.AST.Module
+  , module Language.Haskell.Tools.AST.Lenses
+  ) where
+
+import Language.Haskell.Tools.AST.Ann
+import Language.Haskell.Tools.AST.Base
+import Language.Haskell.Tools.AST.Literals
+import Language.Haskell.Tools.AST.Decl
+import Language.Haskell.Tools.AST.Module
+import Language.Haskell.Tools.AST.Instances
+import Language.Haskell.Tools.AST.Lenses
---
+++ src/ast/Language/Haskell/Tools/AST/Lenses.hs
@@ -0,0 +1,2 @@
+module Language.Haskell.Tools.AST.Lenses where
+
