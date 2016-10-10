c6ab4ca3ce4bbc800a7d1319d3586748716afa27
nboldi <nboldi@elte.hu>
2016-10-10 19:59:37 +0200

    Created a simple bash script for auto deploying.

---
+++ deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+SOURCE_BRANCH="master"
+
+if [ -n "$GITHUB_API_KEY" ]; then
+    echo "No API key given, skipping deploy"
+    exit 0
+fi
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then
+    echo "Skipping deploy"
+    exit 0
+fi
+
+echo "Starting deploy"
+
+# Clone the existing gh-pages for this repo into out/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+git clone https://github.com/haskell-tools/haskell-tools.github.io out
+cd out
+git checkout master
+
+# Clean out existing contents
+rm -rf **/* || exit 0
+
+# Copy generated haddock documentation
+cp .stack-work/install/x86_64-linux/nightly-2016-09-10/8.0.1/doc api
+
+# Now let's go have some fun with the cloned repo
+git config user.name "haskell-tools"
+git config user.email "nboldi@caesar.elte.hu"
+
+# Commit the "changes", i.e. the new version.
+# The delta will show diffs between new and old versions.
+git add .
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+
+git push -f -q https://haskell-tools:$GITHUB_API_KEY@github.com/haskell-tools/haskell-tools.github.io &2>/dev/null
