1412e468e005ff7558857f78e2a6bb1af0c82099
nboldi <nboldi@elte.hu>
2016-10-11 18:25:11 +0200

    added a link for the commit

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,7 @@ set -e # Exit with nonzero exit code if anything fails
 
 SOURCE_BRANCH="master"
 
-# if ! [ -n "$GITHUB_API_KEY" ]; then
-#     echo "No API key given, skipping deploy"
-#     exit 0
-# fi
-
-# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+# Pull requests and commits to other branches shouldn't try to deploy
 # if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then
 #     echo "Skipping deploy"
 #     exit 0
@@ -35,12 +30,9 @@ cp -r .stack-work/install/x86_64-linux/nightly-2016-09-10/8.0.1/doc out/api
 cd out
 
 git config user.name "Travis CI"
-git config user.email "nboldi@elte.hu"
+git config user.email "nboldi@caesar.elte.hu"
 git config push.default simple
 
 git add .
-git commit -m "Updating the API documentation"
-
-
-
+git commit -m "Updating the API documentation - see: https://github.com/haskell-tools/haskell-tools/commit/$TRAVIS_COMMIT"
 git push -f
