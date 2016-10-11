cb9266fa42a22eb95c12b6919d72c13e5a3177ed
nboldi <nboldi@elte.hu>
2016-10-11 17:41:58 +0200

    added logging

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,17 @@ SOURCE_BRANCH="master"
 echo "Starting deploy"
 
 openssl aes-256-cbc -K $encrypted_961cdb62d58f_key -iv $encrypted_961cdb62d58f_iv -in id_rsa.enc -out id_rsa -d
+
+echo "key decrypted"
+
 chmod 600 id_rsa
 eval `ssh-agent -s`
+
+echo "ssh-agent"
+
 ssh-add id_rsa
+
+echo "ssh added"
 
 # Clone the existing repo into out/
 git clone git@github.com:haskell-tools/haskell-tools.github.io out
