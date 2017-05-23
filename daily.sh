b63e758b44e1132d0f64896257313d291e986a4f
nboldi <nboldi@elte.hu>
2017-05-23 11:42:52 +0200

    Try to put into a separate file.

---
+++ daily.sh
@@ -0,0 +1,17 @@
+#if [ "$TRAVIS_EVENT_TYPE" = "cron" ];
+#then
+# Run benchmarks. Need to uninstall first to run this without coverage reporting, so the result will be more accurate.
+#travis_wait stack --no-terminal bench haskell-tools-cli > benchmark.txt 2>&1
+stack sdist
+for PKG in ast backend-ghc cli daemon debug prettyprint refactor rewrite
+do
+  cp `find src/${PKG}/.stack-work -name haskell-tools-${PKG}-*.tar.gz` .
+  rm -r src/${PKG}/*
+  tar -zxf haskell-tools-${PKG}-*.tar.gz -C src/${PKG}/*
+done
+stack --no-terminal test --coverage haskell-tools-rewrite
+stack --no-terminal test --coverage haskell-tools-refactor
+stack --no-terminal test --coverage haskell-tools-cli
+stack --no-terminal test --coverage haskell-tools-daemon
+stack --no-terminal test --coverage haskell-tools-demo
+#fi
