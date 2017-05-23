b54ed731cc6c0bc7e78108ea7e70962bc9c79fa0
nboldi <nboldi@elte.hu>
2017-05-23 17:18:29 +0200

    Removed logging, put daily to the test phase.

--- daily.sh
+++ daily.sh
@@ -1,13 +1,14 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
 #if [ "$TRAVIS_EVENT_TYPE" = "cron" ];
 #then
-# Run benchmarks. Need to uninstall first to run this without coverage reporting, so the result will be more accurate.
-#travis_wait stack --no-terminal bench haskell-tools-cli > benchmark.txt 2>&1
 for PKG in ast backend-ghc cli daemon debug prettyprint refactor rewrite
 do
-  echo "Testing distribution of ${PKG}"
-  cp -v `find src/${PKG}/.stack-work -name haskell-tools-${PKG}-*.tar.gz` .
+  echo "Extracting the distribution of ${PKG}"
+  cp `find src/${PKG}/.stack-work -name haskell-tools-${PKG}-*.tar.gz` .
   rm -r src/${PKG}/*
-  tar -zvx -f haskell-tools-${PKG}-*.tar.gz
+  tar -zx -f haskell-tools-${PKG}-*.tar.gz
   mv haskell-tools-${PKG}-*/* src/${PKG}
 done
 echo "Running tests on the extracted folders"
