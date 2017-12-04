db17a0e9bda3371425be0ec13919d31c96d33ef3
Anabra <peter.d.podlovics@gmail.com>
2017-12-04 07:55:56 +0100

    Changed extra dependecy of ClassyPlate from GitHub to Hackage
    # Conflicts:
    #	src/debug/haskell-tools-debug.cabal
    #	stack.yaml

--- src/builtin-refactorings/Language/Haskell/Tools/Refactor/Builtin/ExtensionOrganizer/TraverseAST.hs
+++ src/builtin-refactorings/Language/Haskell/Tools/Refactor/Builtin/ExtensionOrganizer/TraverseAST.hs
@@ -9,8 +9,8 @@ import Data.Generics.ClassyPlate
 
 import Language.Haskell.Tools.Refactor
 import Language.Haskell.Tools.Refactor.Builtin.ExtensionOrganizer.ExtMonad
-import Language.Haskell.Tools.Refactor.Builtin.ExtensionOrganizer.Instances.Checkable
-import Language.Haskell.Tools.Refactor.Builtin.ExtensionOrganizer.Instances.AppSelector
+import Language.Haskell.Tools.Refactor.Builtin.ExtensionOrganizer.Instances.Checkable()
+import Language.Haskell.Tools.Refactor.Builtin.ExtensionOrganizer.Instances.AppSelector()
 
 
 traverseModule :: CheckNode UnnamedModule
