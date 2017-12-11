4e864ec539d08124fba3e13437717617d4bef1e4
nboldi <nboldi@elte.hu>
2017-12-11 09:40:23 +0900

    Added the FindUnused refactoring to experimental ones.

---
+++ src/experimental-refactorings/Language/Haskell/Tools/Refactor/Builtin/FindUnused.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE TypeFamilies, FlexibleContexts, TemplateHaskell, TupleSections #-}
+module Language.Haskell.Tools.Refactor.Builtin.FindUnused where
+
+import Language.Haskell.Tools.PrettyPrint (prettyPrint)
+import Language.Haskell.Tools.Refactor
+
+import GHC
+import Name
+import Data.Set (Set, empty, (\\), toList, insert)
+import Data.List (intercalate, groupBy)
+import Data.Function (on)
+import Control.Reference
+import Control.Monad.State
+import Debug.Trace (trace)
+import Outputable (ppr, showSDocUnsafe)
+
+data UnusedState = UnusedState { _defined :: Set GHC.Name
+                               , _used :: Set GHC.Name
+                               }
+
+initState = UnusedState empty empty
+
+makeReferences ''UnusedState
+
+findUnusedRefactoring :: RefactoringChoice
+findUnusedRefactoring = ProjectRefactoring "FindUnused" findUnused
+
+findUnused :: ProjectRefactoring
+findUnused mods = do let (mod', st) = flip runState initState $ mapM (biplateRef !~ recordName) (map snd mods)
+                         unused = toList ((st ^. defined) \\ (st ^. used))
+                     unusedDefs <- filterM (fmap not . isRecordName) unused
+                     let groupedUnused = groupBy ((==) `on` nameModule_maybe) unusedDefs
+                     liftIO $ putStrLn $ "Unused definitions:"
+                     liftIO $ putStrLn
+                            $ unlines
+                            $ map (\ls -> (showSDocUnsafe $ ppr $ nameModule_maybe (head ls)) ++ ": "
+                                             ++ intercalate ", " (map (showSDocUnsafe . ppr) ls)) 
+                            $ groupedUnused
+                     return [] -- no changes
+  where isRecordName n = do tt <- lookupName n
+                            case tt of Just (AnId id) -> return $ isRecordSelector id
+                                       _ -> return False
+
+recordName :: QualifiedName -> State UnusedState QualifiedName
+recordName n = do let name = semanticsName n
+                      isDefined = semanticsDefining n 
+                  case name of Just semaName -> if isDefined then modify $ defined .- insert semaName
+                                                             else modify $ used .- insert semaName
+                               Nothing -> return ()
+                  return $ trace ("\n### Name: " ++ prettyPrint n ++ " " ++ show isDefined) $ n
