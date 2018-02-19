63781faf473a4477cae6037b47b0e33642cb873a
Anabra <peter.d.podlovics@gmail.com>
2018-02-19 21:59:19 +0100

    Generalized the parser for the ExtensionOrganizer test-suite

--- src/builtin-refactorings/test/ExtensionOrganizerTest/AnnotationParser.hs
+++ src/builtin-refactorings/test/ExtensionOrganizerTest/AnnotationParser.hs
@@ -1,10 +1,14 @@
 module ExtensionOrganizerTest.AnnotationParser
-  (getLocalExtensionAnnotations, getGlobalExtensionAnnotations)
+  -- (getLocalExtensionAnnotations, getGlobalExtensionAnnotations)
   where
+
+import ExtensionOrganizerTest.Parser
 
 import Data.List
 import qualified Data.Map.Strict as SMap (Map, empty, insertWith)
+import Language.Haskell.Tools.Refactor.Utils.Extensions (canonExt)
 import Language.Haskell.Tools.Refactor.Builtin.ExtensionOrganizer.ExtMap
+
 
 {-# ANN module "HLint: ignore Use zipWith" #-}
 
@@ -26,11 +30,10 @@ parseLine :: (Int, String) -> (Int, [LogicalRelation Extension])
 parseLine (num, line) = (num, parseLocalAnnot . getLocalAnnot $ line)
 
 parseLocalAnnot :: String -> [LogicalRelation Extension]
-parseLocalAnnot = map parseRelation . delimit (== ',')
+parseLocalAnnot = map parseRelation . prepareAnnot
 
--- NOTE: Currently only parses OR relation
 parseRelation :: String -> LogicalRelation Extension
-parseRelation = foldl1 (:||:) . map (lVar . read) . delimit (== '+')
+parseRelation = execParser relation
 
 parseGlobalAnnot :: String -> [Extension]
 parseGlobalAnnot = map read . delimit (== ',')
@@ -54,8 +57,34 @@ delimit' _ [] acc = acc
 delimit' pred xs acc = delimit' pred l2 (acc ++ [l1])
   where (l1, l2) = break' pred xs
 
--- exludes the elemnt for which the predicate is true
+-- exludes the element for which the predicate is true
 break' _ xs@[] = (xs, xs)
 break' p (x:xs')
   | p x        = ([],xs')
   | otherwise  = let (ys,zs) = break' p xs' in (x:ys,zs)
+
+---------
+
+prepareAnnot :: String -> [String]
+prepareAnnot = map (filter (not . isSpace)) .  delimit (== ',')
+
+crop :: String -> String
+crop = dropWhile isSpace . reverse . dropWhile isSpace . reverse
+
+-- | A relation parser that tries to reduce the left-hand side of operators,
+-- before applíing the left-recursive rules.
+-- Also the rules are a bit convoluted in order to respect the precedence
+-- of the operators
+relation :: Parser (LogicalRelation Extension)
+relation = primRel
+       <|> (:&&:) <$> primRel  <*> (token "*" *> primRel)
+       <|> (:||:) <$> primRel  <*> (token "+" *> relation)
+       <|> (:||:) <$> relation <*> (token "+" *> relation)
+       <|> (:&&:) <$> relation <*> (token "*" *> relation)
+       <|> Not    <$> (token "~" *> relation)
+
+  where primRel = (lVar . readExt) <$> word
+              <|> Not <$> (token "~" *> primRel)
+
+        readExt :: String -> Extension
+        readExt = read . canonExt
